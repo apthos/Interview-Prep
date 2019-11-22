9517e578c1bfab74b9d5712a88aaaf657c2af425
apthos <david00lara@gmail.com>
2019-11-22 01:33:44 -0500

    Added solution to 448

--- LeetCode/448_FindDisappearedNumbers.java
+++ LeetCode/448_FindDisappearedNumbers.java
@@ -1,17 +1,26 @@
 class Solution {
+    /*
+    Runtime: 6 ms
+    Time complexity: O(n)
+    Space complexity: O(1) (no extra space)
+    */
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i = 1; i <= nums.length; i++) {
-            list.add(i);
-        }
-        
-        for(int i = 0; i < nums.length; i++){
-            if(list.contains(nums[i])) {
-                list.remove(list.indexOf(nums[i]));
+        //Negate the values at the indexes that have appeared
+        //Example: i = 0 so look at nums[0]. nums[0] = 4 so 4 appears in the array. Go to nums[3] and, if positive, negate it to mark index 3 (4th value) as appeared.
+        for(int i = 0; i < nums.length; i++) {
+            //Ignore if already marked as appeared
+            if(nums[Math.abs(nums[i]) - 1] > 0) {
+                nums[Math.abs(nums[i]) - 1] *= -1;
             }
         }
         
-        return list;
+        List<Integer> disappeared = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                disappeared.add(i + 1);
+            }
+        }
+        
+        return disappeared;
     }
 }
